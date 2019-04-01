88887e9e0c74481733770373d6f1ea0189b9c591
Yury Alencar Lima <yuryalencar19@gmail.com>
2019-04-01 12:26:46 -0300

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -11,26 +11,10 @@ class Role extends Model
     ];
 
     public static $superUser = "#system.root#";
-    public static $entrepreneur = "entrepreneur";
-
-    public static function getSuperUser()
-    {
-        return self::where("name", self::$superUser)->first();
-    }
 
     public function scopeNotSuperProfile($query)
     {
         return $query->where('name', '<>', self::$superUser);
     }
 
-    public static function getEntrepreneur()
-    {
-        return self::where("name", self::$entrepreneur)->first();
-    }
-
-    public
-    function users()
-    {
-        return $this->belongsToMany('App\User', 'user_role')->withTimeStamps();
-    }
 }
