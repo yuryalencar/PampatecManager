dca0af14017deb980b2954715897947c02a296b6
Juliana Mareco <julianamareco18@gmail.com>
2019-04-29 02:17:58 -0300

    UPDATE: adição dos campos #1

--- resources/views/plano_de_negocio/novoplano.blade.php
+++ resources/views/plano_de_negocio/novoplano.blade.php
@@ -3,34 +3,100 @@
 @section('title', 'Novo Plano')
 
 @section('content_header')
+
     <h1>Novo Plano</h1>
     <br>
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
-            <li><a href="">Contatos</a></li>
-            <li><a href="">O Negócio</a></li>
-            <li><a href="">Análise de Mercado</a></li>
-            <li><a href="">Produto ou Serviço</a></li>
-            <li><a href="">Gestão de Pessoas</a></li>
-            <li><a href="">Plano Financeiro</a></li>
+            <li><a id="navContact" onclick="enableContact()">Contatos</a></li>
+            <li><a id="navBusiness" onclick="enableBusiness()">O Negócio</a></li>
+            <li><a id="navMarketAnalysis" onclick="enableMarketAnalysis()">Análise de Mercado</a></li>
+            <li><a id="navProductServer" onclick="enableProductServer()">Produto ou Serviço</a></li>
+            <li><a id="navPeopleManagement" onclick="enablePeopleManagement()">Gestão de Pessoas</a></li>
+            <li><a id="navFinancialPlan" onclick="enableFinancialPlan()">Plano Financeiro</a></li>
         </ol>
     </nav>
 @stop
 
 @section('content')
+
     <div class="box">
 {{--        <div class="box-header">--}}
 {{--        </div>--}}
         <div class="box-body">
             <form>
                 <div class="form-group">
-                    <h4>Empresa/Projeto:</h4>
-                    <input type="text" name="" placeholder="" class="form-control">
+                    <div id="contact">
+                        @include('plano_de_negocio.partials.contacts')
+                    </div>
+                    <div id="business">
+                        @include('plano_de_negocio.partials.business')
+                    </div>
+                    <div id="marketAnalysis">
+                        @include('plano_de_negocio.partials.marketAnalysis')
+                    </div>
+                    <div id="productServer">
+                        @include('plano_de_negocio.partials.productServer')
+                    </div>
+                    <div id="peopleManagement">
+                        @include('plano_de_negocio.partials.peopleManagement')
+                    </div>
+                    <div id="financialPlan">
+                        @include('plano_de_negocio.partials.financialPlan')
+                    </div>
                 </div>
                 <div class="form-group">
-                    <button type="submit" class="btn btn-success">Salvar</button>
+                    <button type="submit" class="btn btn-success pull-right">Salvar</button>
                 </div>
             </form>
         </div>
     </div>
+
+    <script>
+        window.onload = enableContact();
+        function disableAll() {
+            document.getElementById('contact').setAttribute('hidden' , 'true');
+            document.getElementById('navContact').style.fontWeight = 'normal';
+            document.getElementById('business').setAttribute('hidden' , 'true');
+            document.getElementById('navBusiness').style.fontWeight = 'normal';
+            document.getElementById('financialPlan').setAttribute('hidden' , 'true');
+            document.getElementById('navFinancialPlan').style.fontWeight = 'normal';
+            document.getElementById('marketAnalysis').setAttribute('hidden' , 'true');
+            document.getElementById('navMarketAnalysis').style.fontWeight = 'normal';
+            document.getElementById('peopleManagement').setAttribute('hidden' , 'true');
+            document.getElementById('navPeopleManagement').style.fontWeight = 'normal';
+            document.getElementById('productServer').setAttribute('hidden' , 'true');
+            document.getElementById('navProductServer').style.fontWeight = 'normal';
+        }
+        function enableContact() {
+            disableAll();
+            document.getElementById('contact').removeAttribute('hidden');
+            document.getElementById('navContact').style.fontWeight = 'bold';
+        }
+        function enableBusiness() {
+            disableAll();
+            document.getElementById('business').removeAttribute('hidden');
+            document.getElementById('navBusiness').style.fontWeight = 'bold';
+        }
+        function enableFinancialPlan() {
+            disableAll();
+            document.getElementById('financialPlan').removeAttribute('hidden');
+            document.getElementById('navFinancialPlan').style.fontWeight = 'bold';
+        }
+        function enableMarketAnalysis() {
+            disableAll();
+            document.getElementById('marketAnalysis').removeAttribute('hidden');
+            document.getElementById('navMarketAnalysis').style.fontWeight = 'bold';
+        }
+        function enablePeopleManagement() {
+            disableAll();
+            document.getElementById('peopleManagement').removeAttribute('hidden');
+            document.getElementById('navPeopleManagement').style.fontWeight = 'bold';
+        }
+        function enableProductServer() {
+            disableAll();
+            document.getElementById('productServer').removeAttribute('hidden');
+            document.getElementById('navProductServer').style.fontWeight = 'bold';
+        }
+    </script>
 @stop
---
+++ resources/views/plano_de_negocio/partials/business.blade.php
@@ -0,0 +1,8 @@
+<h5 style="display: inline">Segmento de Clientes:</h5> <button style="display: inline" type="button" class="btn btn-link pull-right"><i class="fa fa-question-circle fa-lg" aria-hidden="true"></i></button>
+<textarea rows="5" type="text" name="" placeholder="" class="form-control"></textarea>
+<br/>
+<h5 style="display: inline">Proposta de Valor:</h5> <button style="display: inline" type="button" class="btn btn-link pull-right"><i class="fa fa-question-circle fa-lg" aria-hidden="true"></i></button>
+<textarea rows="5" type="text" name="" placeholder="" class="form-control"></textarea>
+<br/>
+<h5 style="display: inline">Atividades Chave:</h5> <button style="display: inline" type="button" class="btn btn-link pull-right"><i class="fa fa-question-circle fa-lg" aria-hidden="true"></i></button>
+<textarea rows="5" type="text" name="" placeholder="" class="form-control"></textarea>
